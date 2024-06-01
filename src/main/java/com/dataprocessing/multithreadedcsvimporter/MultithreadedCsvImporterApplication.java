3eb20bf6fb3cbff82cc33eb62e61e0021cbe296a
M. Boufous <bfsmhmd@gmail.com>
2024-06-01 14:32:04 +0200

    first commit

---
+++ src/main/java/com/dataprocessing/multithreadedcsvimporter/MultithreadedCsvImporterApplication.java
@@ -0,0 +1,13 @@
+package com.dataprocessing.multithreadedcsvimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedCsvImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadedCsvImporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dataprocessing/multithreadedcsvimporter/MultithreadedCsvImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataprocessing.multithreadedcsvimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedCsvImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
